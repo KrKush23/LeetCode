b6c5680dfca71364af69cb567afe279278cae4b7
KrKush23 <48709290+KrKush23@users.noreply.github.com>
2020-01-23 13:38:47 +0530

    Create Unique Binary Search Trees II.cpp

---
+++ Recursion/Unique Binary Search Trees II.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if(!n)  return vector<TreeNode*> {};
+        return  helper(1,n);
+    }
+    vector<TreeNode*> helper(int start, int end){
+        vector<TreeNode*> res{};
+        if(start>end){
+            res.push_back(NULL);
+            return res;
+        }
+        if(start==end){
+            res.push_back(new TreeNode(start));
+            return res;
+        }
+        vector<TreeNode*> left{},right{};
+        for(int i=start; i<=end; i++){
+            left=helper(start,i-1);
+            right=helper(i+1,end);
+            for(auto lnode: left){
+                for(auto rnode: right){
+                    auto root= new TreeNode(i);
+                    root->left=lnode;
+                    root->right=rnode;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+};
