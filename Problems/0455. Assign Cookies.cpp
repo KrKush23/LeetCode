9fd991c5939c835979d386d1931fdd142a25a2bc
KrKush23 <48709290+KrKush23@users.noreply.github.com>
2020-04-04 00:49:54 +0530

    Create 0455. Assign Cookies.cpp

---
+++ Problems/0455. Assign Cookies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i{0},j{0};
+        int n = g.size(), m = s.size();
+        while(i<n and j<m){
+            if(g[i] <= s[j++])
+                i++;
+        }
+        return i;
+    }
+};
