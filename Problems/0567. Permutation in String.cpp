f5951e1f217f319bfde0982624e34dd9c52b74d5
KrKush23 <48709290+KrKush23@users.noreply.github.com>
2020-04-15 21:58:59 +0530

    Update 0567. Permutation in String.cpp

--- Problems/0567. Permutation in String.cpp
+++ Problems/0567. Permutation in String.cpp
@@ -4,12 +4,12 @@ public:
         int m = s1.length(), n = s2.length();
         if(m>n) return 0;
         vector<int> m1(26,0), m2(26,0);
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++){       //Creating window of appropriate size
             m1[s1[i]-'a']++;
             m2[s2[i]-'a']++;
         }
         if(m1==m2)  return 1;
-        for(int i=0;i<n-m;i++){
+        for(int i=0;i<n-m;i++){     //Sliding the window
             m2[s2[i+m]-'a']++;
             m2[s2[i]-'a']--;
             if(m1==m2)  return 1;
