49e00bd2dccb17cac8b82b69df6f76271f1815d1
KrKush23 <48709290+KrKush23@users.noreply.github.com>
2020-05-10 16:34:03 +0530

    Create 0997. Find the Town Judge.cpp

---
+++ Problems/0997. Find the Town Judge.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> cnt(n+1,0);
+        for(auto i:trust){
+            cnt[i[1]]++;
+            cnt[i[0]]--;
+        }
+        for(int i=1;i<=n;i++){
+            if(cnt[i]==n-1)
+                    return i;
+        }
+        return -1;
+    }
+};
