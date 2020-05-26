68f304db1c44e48a6267c404f584dca1c9388241
KrKush23 <48709290+KrKush23@users.noreply.github.com>
2020-05-26 22:42:33 +0530

    Update 0525. Contiguous Array.cpp

--- Problems/0525. Contiguous Array.cpp
+++ Problems/0525. Contiguous Array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int,int> m{};
+        unordered_map<int,int> counts{};
         int ans{0},sum{0},n=nums.size();
-        m[0]=-1;
+        counts[0]=-1;
         for(int i=0;i<n;i++){
             sum += (nums[i])? 1:-1;
-            if(m.find(sum)==m.end())
-                m[sum]=i;
+            if(counts.find(sum)==counts.end())
+                counts[sum]=i;  //if that 'sum' wasn't encountered before
             else
-                ans = max(ans, i-m[sum]);
+                ans = max(ans, i-counts[sum]); // if 'sum' was found before
         }
         return ans;
     }
