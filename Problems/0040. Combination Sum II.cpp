f06c12098ba32169a3ed3c7d4c0895f041801a64
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2020-09-13 01:34:05 +0530

    Create 0040. Combination Sum II.cpp

---
+++ Problems/0040. Combination Sum II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res{};
+        vector<int> path{};
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0, res, path);
+        return res;
+    }
+private:
+    void dfs(const vector<int>& candidates, int target, int idx, vector<vector<int>>& res, vector<int>& path){
+        if(target==0){
+            res.push_back(path);
+            return;
+        }
+        for(int i=idx; i<candidates.size(); i++){
+            if(candidates[i] > target)
+                return;
+            // To prevent redundant solutions
+            if(i>idx and candidates[i]==candidates[i-1])
+                continue;
+            path.push_back(candidates[i]);
+            dfs(candidates, target-candidates[i], i+1, res, path);
+            path.pop_back();
+        }
+    }
+};
