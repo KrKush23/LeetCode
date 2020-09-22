1e8b0a36036b20457e1dc8d02f271f288f8bbfd0
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2020-09-22 19:02:30 +0530

    Update 0169. Majority Element.cpp

--- Problems/0169. Majority Element.cpp
+++ Problems/0169. Majority Element.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     //============= BOYER - MOORE ALGORITHM ==============
     int majorityElement(vector<int>& nums) {
-        int ans{nums[0]},cnt{1},n=nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]==ans)
-                cnt++;
-            else
-                cnt--;
-            if(cnt==0){
-                ans=nums[i];
-                cnt=1;
-            }
+        int cnt{0}, ans{};
+        for(auto i:nums){
+            if(cnt==0)
+                ans=i;
+            cnt += (ans == i) ? 1 : -1;
         }
         return ans;
     }
