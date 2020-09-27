672f05256e38407ef22ef7a8fdd4d864dd7a7801
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2020-09-27 19:06:10 +0530

    Update 0399. Evaluate Division.cpp

--- Problems/0399. Evaluate Division.cpp
+++ Problems/0399. Evaluate Division.cpp
@@ -1,23 +1,4 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-
-        // TRANSPOSE
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i < j)
-                    swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-
-        //FLIP HORIZONTALLY
-        for (int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-
-    }
-}; #define pb push_back
+#define pb push_back
 class Solution {
     unordered_map<string, vector<pair<string, double>>> graph{};
 public:
