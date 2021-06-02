f56a822153e8978da68b18c06199794e03f4bc30
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-02 20:56:27 +0530

    Update 0733. Flood Fill.cpp

--- Problems/0733. Flood Fill.cpp
+++ Problems/0733. Flood Fill.cpp
@@ -1,18 +1,31 @@
 class Solution {
+        int n, m;
+        int oldColor;
+        vector<int> dx{0, -1, 0, 1};
+        vector<int> dy{-1, 0, 1, 0};
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int oldColor = image[sr][sc];
-        if(oldColor != newColor)
-            callbfs(image, sr, sc, oldColor, newColor);
-        return image;
+    bool isValid(vector<vector<int>>& image, int i, int j){
+        if(i<0 or j<0 or i>=n or j>=m or image[i][j] != oldColor)
+            return 0;
+        return 1;        
     }
-    void callbfs(vector<vector<int>>& image, int i, int j,int oldColor, int newColor){
-        if(i<0 || j<0 || i >= image.size() || j>= image[i].size() || image[i][j] != oldColor)
-            return;
+    
+    void dfs(vector<vector<int>>& image, int i, int j, int newColor){
         image[i][j] = newColor;
-        callbfs(image, i+1, j  , oldColor, newColor); //down
-        callbfs(image, i  , j+1, oldColor, newColor); //right
-        callbfs(image, i-1, j  , oldColor, newColor); //up
-        callbfs(image, i  , j-1, oldColor, newColor); //left
+        for(int k=0; k<4; k++){
+            if(isValid(image, i+dx[k], j+dy[k]))
+                dfs(image, i+dx[k], j+dy[k], newColor);
+        }
+        
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        n = image.size();
+        m = image[0].size();
+        
+        oldColor = image[sr][sc];
+        if(image[sr][sc] != newColor)
+            dfs(image, sr, sc, newColor);
+        return image;
     }
 };
