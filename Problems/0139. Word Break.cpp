01b9c5274ea78865f3ddcbcedf677ddbb1f3353c
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-10 19:56:07 +0530

    Update 0139. Word Break.cpp

--- Problems/0139. Word Break.cpp
+++ Problems/0139. Word Break.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
-        vector<bool> dp(n+1,0);
-        dp[0]=1;//word can be made up till here from dict
-        set<string> words(wordDict.begin(), wordDict.end());
-        
-        for(int l=0; l<n; l++){
-            if(!dp[l])      //means words starting from here should not be searched for
-                continue;
-            //finding all matching words from starting from l
-            for(int h = l; h<=n; h++){ 
-                string t = s.substr(l, h-l);
-                if(words.find(t) != words.end())
-                    dp[h]=1;
-            }
-        }
-        return dp[n];
-    }
+	bool isPresent(string word, vector<string>& wordDict) {
+		for (int i = 0; i < wordDict.size(); i++) {
+			if (wordDict[i].compare(word) == 0)
+				return true;
+		}
+		return false;
+	}
+
+// BACKTRACKING ====================
+    // TLE
+// 	bool wordBreak(string s, vector<string> &wordDict) {
+// 		int l = s.length();
+// 		if ( l == 0) return 1;
+
+// 		for (int i = 1; i <= l; i++) {
+// 			if (isPresent(s.substr(0, i), wordDict)
+// 			        and  wordBreak(s.substr(i, l - i), wordDict))
+// 				return true;
+// 		}
+// 		return false;
+// 	}
+
+// TABULAR =======================
+	bool wordBreak(string s, vector<string> &wordDict) {
+		int l = s.length();
+		if ( l == 0) return true;
+
+		vector<bool> dp(l + 1, 0);
+		dp[0] = 1;
+
+		for (int i = 0; i < l; i++) {
+			if (!dp[i])
+				continue;
+			for (int j = i + 1; j <= l; j++) {
+				if (isPresent(s.substr(i, j - i), wordDict))
+					dp[j] = 1;
+			}
+		}
+		return dp[l];
+	}
 };
