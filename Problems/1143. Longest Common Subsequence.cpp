d65e00fe5ff03620348d79f2b09f2236dc25b7fb
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-15 01:16:33 +0530

    Update 1143. Longest Common Subsequence.cpp

--- Problems/1143. Longest Common Subsequence.cpp
+++ Problems/1143. Longest Common Subsequence.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length(), m = text2.length();
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
+        // RECURSIVE APPROACH (TLE) =============
+        // ======================================
+        /*
+        // BASE CASE
+        if(n==0 or m==0)
+            return 0;
+        // RECUSRIVE CASE
+        if(text1[n-1] == text2[m-1])
+            return 1 + helper(n-1, m-1, s1, s2);
+        else
+            return max(helper(n-1, m, s1, s2), helper(n, m-1, s1, s2));
+        */
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1])     //*IMPORTANT*
+        // TOP-DOWN APPROACH ===================
+        // =====================================
+        
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(text1[i-1] == text2[j-1])
                     dp[i][j] = 1 + dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
+        
         return dp[n][m];
+        
     }
 };
