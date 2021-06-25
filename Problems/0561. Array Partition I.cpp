7194d94edc0dc2504515b4d64c15bb03dca1e66b
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-25 21:14:41 +0530

    Update 0561. Array Partition I.cpp

--- Problems/0561. Array Partition I.cpp
+++ Problems/0561. Array Partition I.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size(),sum{};
-        for(int i=0;i<n;i+=2)
-            sum+= min(nums[i],nums[i+1]);
-        return sum;
+        sort(nums.begin(), nums.end());
+        
+        int res{0};
+        
+        for(int i=0; i<nums.size(); i+=2)
+            res += nums[i];
+        
+        return res;
     }
 };
