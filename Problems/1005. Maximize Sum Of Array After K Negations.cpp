b20927388bfce9c7a3a1edb7c7b54e05d50253f6
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-27 02:11:04 +0530

    Update 1005. Maximize Sum Of Array After K Negations.cpp

--- Problems/1005. Maximize Sum Of Array After K Negations.cpp
+++ Problems/1005. Maximize Sum Of Array After K Negations.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        int res{};
-        priority_queue<int, vector<int>, greater<int>> pq; //MIN HEAP
-        for(auto i:nums)
-            pq.push(i);
-        
-        while(k--){
-            int temp = pq.top();
-            if(temp == 0)
-                break;
-            pq.pop();
-            pq.push(temp * -1);
+        // BETTER
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while (i < nums.size() and nums[i] < 0 and k > 0) {
+            nums[i] = -nums[i];
+            k--;
+            i++;
         }
+        if (k%2 == 0)
+            return accumulate(nums.begin(), nums.end(), 0);
+        int mn = *min_element(nums.begin(), nums.end());
+        return accumulate(nums.begin(), nums.end(), 0) - 2*mn;
         
-        while(!pq.empty()){
-            res += pq.top();
-            pq.pop();
-        }
-        return res;
+        
+        
+        //MIN HEAP ==========================================
+//         int res{};
+//         priority_queue<int, vector<int>, greater<int>> pq; //MIN HEAP
+//         for(auto i:nums)
+//             pq.push(i);
+        
+//         while(k--){
+//             int temp = pq.top();
+//             if(temp == 0)
+//                 break;
+//             pq.pop();
+//             pq.push(temp * -1);
+//         }
+        
+//         while(!pq.empty()){
+//             res += pq.top();
+//             pq.pop();
+//         }
+//         return res;
     }
 };
