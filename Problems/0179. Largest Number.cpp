b39c31d38fb01eac69caf10d1db369be35333ffc
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-28 22:56:27 +0530

    Update 0179. Largest Number.cpp

--- Problems/0179. Largest Number.cpp
+++ Problems/0179. Largest Number.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) {        
-        sort(nums.begin(), nums.end(), [](int& a, int& b){
-            string s1{""}, s2{""};
-            s1 += to_string(a);
-            s2 += to_string(b);
-            
-            return s1+s2 > s2+s1;
-        });
-        
-        string res{""};
+    string largestNumber(vector<int>& nums) {
+        string res{};
+        vector<string> s{};
         for(auto i:nums)
-            res += to_string(i);
+            s.push_back(to_string(i));
         
-        if(res.length() > 0 and res[0]=='0')  return "0";
+        sort(s.begin(), s.end(), [](string s1, string s2){return s1+s2>s2+s1;});
+        
+        for(auto t:s)
+            res += t;
+        
+        while(res[0] == '0' and res.length() > 1)
+            res.erase(0, 1);
         return res;
     }
 };
