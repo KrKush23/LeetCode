96f562ff83c8b9e6fd3add4761b9dc97d852ea4d
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-06-30 01:22:29 +0530

    Update 0406. Queue Reconstruction by Height.cpp

--- Problems/0406. Queue Reconstruction by Height.cpp
+++ Problems/0406. Queue Reconstruction by Height.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        // Sort in descending order of 'h'
+        // Sort in descending order of 'h' and then increasing order of 'people he/she can see'
         sort(people.begin(), people.end(), [](vector<int> a, vector<int> b){
            return (a[0]>b[0] || (a[0]==b[0] and a[1]<b[1])); 
         });
         vector<vector<int>> ans{};
         for(auto i:people){
-            ans.insert(ans.begin()+i[1],i);
+            ans.insert(ans.begin()+i[1],i); // insert at the position...at which...he can see..no. of people given
         }
         return ans;
     }
