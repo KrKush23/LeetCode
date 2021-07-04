ac9bbddff6ad798e2a2c5cc82cae4cfc7b469246
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-04 23:52:17 +0530

    Create 0684. Redundant Connection.cpp

---
+++ Problems/0684. Redundant Connection.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<int> par;
+    
+    int find(int a){
+        while(a != par[a])
+            a = par[a];
+        return a;
+    }
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        // UNION FIND ===========================
+        vector<int> res{};
+        par.resize(1001); // Parent Array initialisation
+        for(int i=0; i<par.size(); i++)
+            par[i] = i;
+        
+        for(auto e:edges){
+            int u = find(e[0]);
+            int v = find(e[1]);
+            
+            if(u==v)
+                res = e;
+            else
+                par[v] = u;
+        }
+        return res;        
+    }
+};
