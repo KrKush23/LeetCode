7f8cef468b3b0150e8972e010c9492e3ac12afdf
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-09 17:54:31 +0530

    Update 0019. Remove Nth Node From End of List.cpp

--- Problems/0019. Remove Nth Node From End of List.cpp
+++ Problems/0019. Remove Nth Node From End of List.cpp
@@ -9,16 +9,25 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //CREATE dummy
         ListNode *dummy = new ListNode{};
         dummy->next=head;
-        ListNode *first{dummy}, *second{dummy};
-        for(int i=0;i<n;i++)
-            first=first->next;
-        while(first->next){
-            first = first->next;
-            second = second->next;
+        
+        ListNode *fast{dummy}, *slow{dummy};
+        // FAST goes ahead 'n' nodes
+        while(n--)
+            fast=fast->next;
+        
+        // now FAST and SLOW...move 1 step at a time..till FAST is LAST node
+        while(fast->next){
+            fast = fast->next;
+            slow = slow->next;
         }
-        second->next = second->next->next;
-        return dummy->next;//when head will be removed
+        // THEN delete the next of SLOW..and move its pointer accordingly
+        ListNode *node = slow->next;
+        slow->next = slow->next->next;
+        delete node;
+        
+        return dummy->next;
     }
 };
