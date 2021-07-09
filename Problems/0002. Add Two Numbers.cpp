91edab30d7b13771aee187534ed7cae1dd2e6d1c
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-09 18:42:43 +0530

    Update 0002. Add Two Numbers.cpp

--- Problems/0002. Add Two Numbers.cpp
+++ Problems/0002. Add Two Numbers.cpp
@@ -7,27 +7,46 @@
  * };
  */
 class Solution {
+    ListNode* reverseList(ListNode* head){
+        ListNode *cur=head, *prev=NULL, *next=NULL;
+        while(cur!= NULL){ // 4 STEPS in the loop
+            next = cur->next;
+            cur->next = prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //IF nos. are not in reverse
+//         l1 = reverseList(l1);
+//         l2 = reverseList(l2);
+        
         ListNode *dummyHead = new ListNode(0);
         ListNode *cur=dummyHead;
         int carry{0};
         while(l1 != NULL || l2 != NULL){
             int sum = carry;
-            sum += (l1!=NULL) ? l1->val : 0;
-            sum += (l2!=NULL) ? l2->val : 0;
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
             
             carry = sum/10;
             cur->next = new ListNode(sum%10); //create new node
             cur = cur->next;
-            
-            if(l1!=NULL)    l1=l1->next;
-            if(l2!=NULL)    l2=l2->next;
         }
         //AT last
         if(carry>0)
             cur->next = new ListNode(carry);
         
         return dummyHead->next;
+        //IF you need to return no. in reverse
+//         return reverseList(dummy->next);
     }
 };
