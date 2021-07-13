af866f02562f9d8437cda5cc615c2ecf651688b4
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-13 17:30:06 +0530

    Update 0038. Count and Say.cpp

--- Problems/0038. Count and Say.cpp
+++ Problems/0038. Count and Say.cpp
@@ -2,22 +2,30 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        // BASE CASE
         if(n==1)
             return "1";
+        // Recursion
         string s = countAndSay(n-1);
-        string ans{};
+        
+        string res{};
         int cnt{1};
+        
+        // we have to count...and return what we are saying
         for(int i=1;i<s.length();i++){
+            // increase 'cnt' if same digit
             if(s[i]==s[i-1])
                 cnt++;
+            // else store this count with digit...and reset 'cnt' = 1
             else{
-                ans.pb(cnt+'0');
-                ans.pb(s[i-1]);
+                res.pb(cnt+'0');
+                res += s[i-1];
                 cnt=1;
             }
         }
-        ans.pb(cnt+'0');
-        ans.pb(s[s.length()-1]);
-        return ans;
+        // at last...store cnt and digit of last no.
+        res.pb(cnt+'0');
+        res += s[s.length()-1];
+        return res;
     }
 };
