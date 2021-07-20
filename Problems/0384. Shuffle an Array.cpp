58bd9d486f9c800179643a14be96ffdc3422724f
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-20 16:51:05 +0530

    Create 0384. Shuffle an Array.cpp

---
+++ Problems/0384. Shuffle an Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<int> nums;
+public:
+    Solution(vector<int>& nums) {
+        this->nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> shuffled = nums;
+        
+        //FISHER - YATES algo
+        for(int i=shuffled.size(); i>0; i--)
+            swap(shuffled[i-1], shuffled[rand()%i]);
+        
+        return shuffled;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
