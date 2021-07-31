6e902f94a8380dd95676790a288a0593c7e806cd
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-07-31 21:21:18 +0530

    Update 0042. Trapping Rain Water.cpp

--- Problems/0042. Trapping Rain Water.cpp
+++ Problems/0042. Trapping Rain Water.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // Dynamic Programming - SPACE - O(n)
+        /*
         if(height.size()==0)
             return 0;
         
@@ -20,5 +22,29 @@ public:
             res += min(left[i], right[i]) - height[i];
         
         return res;
+        */
+        
+        // 2 POINTERS - Space - O(1)
+        int total = 0;
+        int left = 0, right = height.size()-1;
+        int left_max = 0, right_max = 0;
+        
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= left_max)
+                    left_max = height[left];
+                else
+                    total += left_max - height[left];
+                left++;
+            }else{
+                if(height[right] >= right_max)
+                    right_max = height[right];
+                else
+                    total += right_max - height[right];
+                right--;
+            }
+        }
+        
+        return total;
     }
 };
