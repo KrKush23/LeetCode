f2c44c513800904433e73ca6864c1d98621c7da0
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-08-02 00:51:28 +0530

    Update 0207. Course Schedule.cpp

--- Problems/0207. Course Schedule.cpp
+++ Problems/0207. Course Schedule.cpp
@@ -2,31 +2,40 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses, vector<int> {});    //adjacency list
-        vector<int> in(numCourses,0);                           //list of in-degrees
+        vector<vector<int>> adj(numCourses, vector<int> {});
+        vector<int> in(numCourses,0);
+        
+        // create adjacency list and indegree VECTORS
         for(auto i:prerequisites){
-            adj[i[0]].pb(i[1]);
-            in[i[1]]++;
+            adj[i[1]].pb(i[0]);
+            in[i[0]]++;
         }
+        
+        // topological sort
         return kahn(numCourses, adj, in);
     }
-    //=============== TOPOLOGICAL SORT ======================
+    
     bool kahn(int n, vector<vector<int>>& adj, vector<int>& in){
         vector<int> res{};
         queue<int> q{};
+        
         for(int i=0;i<n;i++)
-            if(in[i]==0)
+            if(in[i]==0) // take nodes with '0' INDEGREE
                 q.push(i);
+        
         while(!q.empty()){
             int cur = q.front();
             res.pb(cur);
             q.pop();
+            
             for(int child:adj[cur]){ 
-                in[child]--;
-                if(in[child]==0)
+                in[child]--;       // reduce indegree of NEIGHBOURS
+                if(in[child]==0)   // take it if INDEGREE becomes '0'
                     q.push(child);
             }
         }
+        
         return res.size() == n; 
+        // if less than 'n', means deadlock -> we can't finish all courses
     }
 };
