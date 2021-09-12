b370705f65a470708c96ec19bbb5bbeb31b859eb
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-09-12 13:58:53 +0530

    Update 0567. Permutation in String.cpp

--- Problems/0567. Permutation in String.cpp
+++ Problems/0567. Permutation in String.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int m = s1.length(), n = s2.length();
-        if(m>n) return 0;
-        vector<int> m1(26,0), m2(26,0);
-        for(int i=0;i<m;i++){       //Creating window of appropriate size
+        if(m>n) 
+            return 0;
+        
+        vector<int> m1(26), m2(26);
+        //Creating window of appropriate size
+        for(int i=0;i<m;i++){
             m1[s1[i]-'a']++;
             m2[s2[i]-'a']++;
         }
-        if(m1==m2)  return 1;
-        for(int i=0;i<n-m;i++){     //Sliding the window
-            m2[s2[i+m]-'a']++;
-            m2[s2[i]-'a']--;
+        
+        //Sliding the window
+        for(int i=m;i<n;i++){
             if(m1==m2)  return 1;
+            
+            m2[s2[i-m]-'a']--;
+            m2[s2[i]-'a']++;
         }
-        return 0;
+        return m1 == m2;
     }
 };
