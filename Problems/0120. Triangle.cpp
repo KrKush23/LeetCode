8d79d4e8acf0c55da1ad9606b761376a2ad4bb22
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-09-18 15:43:52 +0530

    Update 0120. Triangle.cpp

--- Problems/0120. Triangle.cpp
+++ Problems/0120. Triangle.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        if(triangle.empty())
-            return 0;
-        int m = triangle.size(), n = (m>0)?triangle[m-1].size():0;
-        vector<vector<int>> dp(m, vector<int> (n,0));
-        for(int i=0;i<n;i++)
-            dp[m-1][i] = triangle[m-1][i];
-        for(int i=m-2; i>=0; i--){
-            for(int j=0; j<i+1; j++){
-                dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
+        int n = triangle.size();
+        vector<int> dp(n); // 1D
+        dp[0] = triangle[0][0];
+        
+        for(int k=1; k<n; k++){
+            for(int i=k; i>=0; i--){
+                if(i==0)
+                    dp[i] = dp[i] + triangle[k][i];
+                else if(i==k)
+                    dp[i] = dp[i-1] + triangle[k][i];
+                else
+                    dp[i] = min(dp[i], dp[i-1]) + triangle[k][i];
             }
         }
-        return dp[0][0];
+        
+        return *min_element(dp.begin(), dp.end());
     }
 };
