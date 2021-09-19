28f7ab55fedb42e554e411661e3c1d178c177443
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-09-19 21:30:25 +0530

    Update 0769. Max Chunks To Make Sorted.cpp

--- Problems/0769. Max Chunks To Make Sorted.cpp
+++ Problems/0769. Max Chunks To Make Sorted.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
+        // SOLUTION 1 =============================
+        stack<int> st;
+        
+        for(auto i:arr){
+            int curMax = (st.empty()) ? i : max(i, st.top());
+            
+            while(!st.empty() and st.top() > i)
+                st.pop();
+            
+            st.push(curMax);
+        }
+        return st.size();
+        
+        // SOLUTION 2 =============================
         int cnt{0}, curMax{-1};
         
         for(int i=0; i<arr.size(); i++){
