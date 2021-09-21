a73c58af9e463f6c1424976bcb17974c3d0dc399
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-09-21 19:48:34 +0530

    Update 0057. Insert Interval.cpp

--- Problems/0057. Insert Interval.cpp
+++ Problems/0057. Insert Interval.cpp
@@ -1,24 +1,23 @@
-#define pb push_back
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res{};
-        for(auto i:intervals){
-            // newInterval is after the current interval
-            if(i[1] < newInterval[0])
-                res.pb(i);
-            // newInterval is before the current interval
-            else if(i[0] > newInterval[1]){
-                res.pb(newInterval);
-                newInterval = i;
-            }
-            // overlapping of intervals
-            else{
-                newInterval[0] = min(newInterval[0], i[0]);
-                newInterval[1] = max(newInterval[1], i[1]);
-            }
+        
+        int i{}, n = intervals.size();
+        
+        while(i<n and intervals[i][1] < newInterval[0])
+            res.push_back(intervals[i++]);
+        
+        while(i<n and intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        res.pb(newInterval);
-        return res;        
+        res.push_back(newInterval);
+        
+        while(i<n)
+            res.push_back(intervals[i++]);
+        
+        return res;
     }
 };
