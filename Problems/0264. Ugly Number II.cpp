c093fc3ec4f35753fdf4ac51ba1bb00dae7a4d8d
Kumar Kush <48709290+KrKush23@users.noreply.github.com>
2021-09-22 14:47:30 +0530

    Update 0264. Ugly Number II.cpp

--- Problems/0264. Ugly Number II.cpp
+++ Problems/0264. Ugly Number II.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> nums{1};
-        int idx_2{}, idx_3{}, idx_5{};
-        int next2{}, next3{}, next5{};
-        while(nums.size()<n){
-            next2 = nums[idx_2] * 2;
-            next3 = nums[idx_3] * 3;
-            next5 = nums[idx_5] * 5;
-            int next = min({next2, next3, next5});
-            nums.push_back(next);
-            if(next == next2)
-                idx_2++;
-            if(next == next3)
-                idx_3++;
-            if(next == next5)
-                idx_5++;
+        int dp[n];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        
+        int idx_2{1}, idx_3{1}, idx_5{1};
+        int next2{2}, next3{3}, next5{5};
+        
+        for(int i=1; i<n; i++){
+            dp[i] = min({next2, next3, next5});
+            
+            if (dp[i] == next2) next2 = dp[idx_2++] * 2;
+            if (dp[i] == next3) next3 = dp[idx_3++] * 3;
+            if (dp[i] == next5) next5 = dp[idx_5++] * 5;
         }
-        return nums.back();
+        
+        return dp[n-1];
     }
 };
